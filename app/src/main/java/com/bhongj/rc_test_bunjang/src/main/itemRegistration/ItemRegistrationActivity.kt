c9bb70304572a1b721348e2f77da02b7a84791fb
bhong <wjdghdqo3@naver.com>
2022-03-29 01:26:06 +0900

    add itemRegistration layout

---
+++ app/src/main/java/com/bhongj/rc_test_bunjang/src/main/itemRegistration/ItemRegistrationActivity.kt
@@ -0,0 +1,14 @@
+package com.bhongj.rc_test_bunjang.src.main.itemRegistration
+
+import android.os.Bundle
+import com.bhongj.rc_test_bunjang.config.BaseActivity
+import com.bhongj.rc_test_bunjang.databinding.ActivityItemRegistrationBinding
+
+class ItemRegistrationActivity :
+    BaseActivity<ActivityItemRegistrationBinding>(ActivityItemRegistrationBinding::inflate) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
