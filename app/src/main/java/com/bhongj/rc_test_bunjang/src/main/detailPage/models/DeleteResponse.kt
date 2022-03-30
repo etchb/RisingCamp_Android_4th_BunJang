b43d744bba0cddb738c30f9ee77cec6107251799
bhong <wjdghdqo3@naver.com>
2022-03-31 01:10:10 +0900

    apply itemDelete API

---
+++ app/src/main/java/com/bhongj/rc_test_bunjang/src/main/detailPage/models/DeleteResponse.kt
@@ -0,0 +1,8 @@
+package com.bhongj.rc_test_bunjang.src.main.detailPage.models
+
+data class DeleteResponse(
+    val code: Int,
+    val isSuccess: Boolean,
+    val message: String,
+    val result: String
+)
